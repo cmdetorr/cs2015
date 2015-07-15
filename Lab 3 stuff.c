6fb84e6de5d215818915b4fea2f115fcddf8518a
cmdetorr <cmdetorr@gmail.com>
2015-07-14 23:00:10 -0700

    i've made a changes to the file by adding a comment

--- Lab 3 stuff.c
+++ Lab 3 stuff.c
@@ -21,6 +21,8 @@ board) and converts it to a proportional speed that is applied to a 9V DC motor.
 
 */
 
+// I've made a change to this
+
 const unsigned char seven_seg[16] = {0x3F,0x06, 0x5B, 0x4F, 0x66,
                                      0x6D, 0x7D, 0x07, 0x7F, 0x6F, 
 									 0x77, 0x7C, 0x58, 0x5E, 0x79, 0x71};
